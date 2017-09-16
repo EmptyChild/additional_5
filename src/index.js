8671fec2d67e1c72f1b6c3732c310d00c9d250fd
Nikita Mikhaliuk <nikkita1992@mail.ru>
2017-09-16 16:06:49 +0300

    1st commit, 1st solution, tests is ok, and that's weird)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strArr = str.split('');
+    let brOpeners = [];
+    let brClosers = [];
+    bracketsConfig.forEach(function(element) {
+        brOpeners.push(element[0]);
+        brClosers.push(element[1]);
+    }, this);
+    let checkingArr = [];
+    strArr.forEach(function(element) {
+        if (brOpeners.includes(element) && !brClosers.includes(element)) {
+            checkingArr.push(element);
+        }
+        if (brClosers.includes(element)) {
+            if (checkingArr[checkingArr.length - 1] == brOpeners[brClosers.indexOf(element)]) {
+                checkingArr.pop();
+            } else {
+                checkingArr.push(element);
+            }
+        }
+    }, this);
+    return !checkingArr.join("");
 }
